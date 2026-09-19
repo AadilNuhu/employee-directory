013b7f6e5451a63e6fee55e04d369b51cc5c96dd
Aadil Nuhu <aadilnuhu122@gmail.com>
2026-09-19 11:35:03 +0000

    update handleChange to support HTMLSelectElement in AddEmployee component

--- client/components/AddEmployee.tsx
+++ client/components/AddEmployee.tsx
@@ -15,7 +15,7 @@ const AddEmployee = ({ onClose }: employeeFormProps) => {
     phone: "",
   })
 
-  const handleChange =(e:React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange =(e:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const {name, value} = e.target
 
     setForm((pre) => ({
